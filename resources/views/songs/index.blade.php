757bbe0205205280e35d91f3b4a27f55db2ab27e
Manoj.Rai <2020000014@student.sit.ac.nz>
2021-06-09 15:52:17 +1200

    url https enabled

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -10,7 +10,7 @@
           <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="https://images.unsplash.com/photo-1607274217259-4c20741b71c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{{$song->name}}</text></svg> -->
 
           <div class="carousel-item active">
-            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="https://{{$song->url}}" alt="">
+            <img class="bd-placeholder-img card-img-top" width="100%" height="225" src="{{$song->url}}" alt="">
             <div class="carousel-caption">
                 <h3>{{$song->name}}</h3>
             </div>
